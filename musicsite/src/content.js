33ce9b0341bbc194d3d98bccb13460ae8da27303
erostark <eioncontaste@ac.c4q.nyc>
2018-08-05 11:21:09 -0400

    changed content

--- musicsite/src/content.js
+++ musicsite/src/content.js
@@ -91,27 +91,31 @@ class Content extends React.Component {
     console.log("normal", commentCopy, "reverse",commentCopy.reverse())
     return(
       <div className ="content">
-       <div className="embeds">
-          <iframe src="https://open.spotify.com/embed?uri=spotify:album:1pESV3wn7IZo2ywVb1HFjf" 
-                width="300" height="80" 
-                frameborder="0" allowtransparency="true" 
-                allow="encrypted-media">
-          </iframe>
+       <div>
+        <div className="cover">
+        <iframe width="770" height="433.125" src="https://www.youtube.com/embed/TXSxwfJmvr8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
 
-          <iframe allow="autoplay *; encrypted-media *;" 
-                frameborder="0" height="450" 
-                sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/the-m-o-single/1418312884?app=music" 
-                width="300" height="200">
-          </iframe>
+        <div className="embeds">
+            <iframe src="https://open.spotify.com/embed?uri=spotify:album:1pESV3wn7IZo2ywVb1HFjf" 
+                  width="250" height="80" 
+                  frameborder="0" allowtransparency="true" 
+                  allow="encrypted-media">
+            </iframe>
 
-          <iframe width="300" height="166" 
-                scrolling="no" frameborder="no" allow="autoplay" 
-                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/477900072&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true">
-          </iframe>
-       </div> 
-        
-       <div className="cover">
-          <img src="http://i66.tinypic.com/25ic75v.jpg" alt="Smiley face" height="400" width="400" />
+            <iframe allow="autoplay *; encrypted-media *;" 
+                  frameborder="0" height="450" 
+                  sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/the-m-o-single/1418312884?app=music" 
+                  width="250" height="80">
+            </iframe>
+
+            <iframe width="250" height="80" 
+                  scrolling="no" frameborder="no" allow="autoplay" 
+                  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/477900072&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true">
+            </iframe>
+        </div> 
+          
+
        </div>
         
        <div className="submit container-1">
